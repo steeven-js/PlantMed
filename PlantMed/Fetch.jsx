471c0c1a64356a26779b81ba137f7dbf8ff91c26
steeven-js <jacques.steeven@gmail.com>
2023-10-24 15:36:48 -0400

    Pagination

--- PlantMed/Fetch.jsx
+++ PlantMed/Fetch.jsx
@@ -1,5 +1,11 @@
 import React, {useEffect, useState} from 'react';
-import {ActivityIndicator, FlatList, Text, View} from 'react-native';
+import {
+  ActivityIndicator,
+  FlatList,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import PlantItem from './Components/PlantItem';
 import {apiUrl} from './Common/const';
 import Header from './Header';
@@ -8,16 +14,34 @@ import SearchBar from './Components/SearchBar';
 const Fetch = () => {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [prevPageUrl, setPrevPageUrl] = useState(null);
+  const [nextPageUrl, setNextPageUrl] = useState(null);
 
-  const getPlant = async () => {
+  const getPlant = async url => {
     try {
-      const response = await fetch(apiUrl);
+      const response = await fetch(url || apiUrl);
       const json = await response.json();
-      setData(json);
+      setData(json.plantes);
+      setPrevPageUrl(json.prev_page_url);
+      setNextPageUrl(json.next_page_url);
     } catch (error) {
       console.error(error);
     } finally {
       setLoading(false);
+    }
+  };
+
+  const goToPrev = () => {
+    if (prevPageUrl) {
+      console.log('prevPageUrl', prevPageUrl);
+      getPlant(prevPageUrl);
+    }
+  };
+
+  const goToNext = () => {
+    if (nextPageUrl) {
+      console.log('nextPageUrl', nextPageUrl);
+      getPlant(nextPageUrl);
     }
   };
 
@@ -28,14 +52,14 @@ const Fetch = () => {
   }, []);
 
   return (
-    <View style={{flex:1}}>
+    <View style={{flex: 1}}>
       <View>
         <Header />
       </View>
       <View>
-        <SearchBar/>
+        <SearchBar />
       </View>
-      <View style={{flex: 1, alignItems:'center'}}>
+      <View style={{flex: 1, alignItems: 'center'}}>
         {isLoading ? (
           <ActivityIndicator />
         ) : (
@@ -46,6 +70,25 @@ const Fetch = () => {
             numColumns={2}
           />
         )}
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          paddingRight: 50,
+          alignItems: 'center',
+          justifyContent: 'flex-end',
+        }}>
+        {prevPageUrl ? (
+          <TouchableOpacity style={{padding: 10}} onPress={goToPrev}>
+            <Text style={{padding: 10}}>Prev</Text>
+          </TouchableOpacity>
+        ) : null}
+
+        {nextPageUrl ? (
+          <TouchableOpacity style={{padding: 10}} onPress={goToNext}>
+            <Text style={{padding: 10}}>Next</Text>
+          </TouchableOpacity>
+        ) : null}
       </View>
     </View>
   );
