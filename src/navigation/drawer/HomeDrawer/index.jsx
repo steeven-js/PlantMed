2e66a38bf1f4065c33827ed9454df969aec3d212
steeven-js <jacques.steeven@gmail.com>
2024-02-21 15:43:51 -0400

    Mise à jour de la version de l'application à 1.0.7

--- src/navigation/drawer/HomeDrawer/index.jsx
+++ src/navigation/drawer/HomeDrawer/index.jsx
@@ -70,7 +70,7 @@ const CustomDrawerContent = props => {
             {/* Custom drawer item */}
             <View>
                 <DrawerItem
-                    label="App Version 1.0.6 - Fev, 2024"
+                    label="App Version 1.0.7 - Fev, 2024"
                     labelStyle={[
                         styles.drawerItemLabel,
                         { color: COLORS.textLowContrast, alignSelf: 'center' },
