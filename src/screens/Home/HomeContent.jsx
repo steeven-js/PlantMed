7630ec035c3f60e3880950c5db07cff825ad0ced
steeven-js <jacques.steeven@gmail.com>
2024-04-05 23:28:11 -0400

    Mise à jour des sections de bannière dans le composant HomeContent

--- src/screens/Home/HomeContent.jsx
+++ src/screens/Home/HomeContent.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import { View } from 'react-native';
 
-import HomeBannerSection2 from '../HomeBannerSection2';
-import HomeBestSellerView from '../HomeBestSellerView';
 import HomeCategoriesSection from '../HomeCategories';
-import HomeGlobalOutletsView from '../HomeGlobalOutletsView';
 import HomeMostPopularView from '../HomeMostPopularView';
-import HomeNewArrivalsView from '../HomeNewArrivalsView';
 import HomeRecentlyViewedView from '../HomeRecentlyViewedView';
 import HomeData from '../../data/HomeData';
 
@@ -15,15 +11,15 @@ const HomeContent = ({ theme }) => {
     return (
         <View>
             <HomeCategoriesSection theme={theme} homeData={homeData} />
-            <HomeBannerSection2 theme={theme} />
+            {/* <HomeBannerSection2 theme={theme} /> */}
             <HomeMostPopularView theme={theme} homeData={homeData} />
-            <HomeBannerSection2 theme={theme} />
-            <HomeBestSellerView theme={theme} homeData={homeData} />
-            <HomeBannerSection2 theme={theme} />
-            <HomeNewArrivalsView theme={theme} homeData={homeData} />
-            <HomeBannerSection2 theme={theme} />
+            {/* <HomeBannerSection2 theme={theme} /> */}
+            {/* <HomeBestSellerView theme={theme} homeData={homeData} /> */}
+            {/* <HomeBannerSection2 theme={theme} /> */}
+            {/* <HomeNewArrivalsView theme={theme} homeData={homeData} /> */}
+            {/* <HomeBannerSection2 theme={theme} /> */}
             <HomeRecentlyViewedView theme={theme} homeData={homeData} />
-            <HomeGlobalOutletsView theme={theme} homeData={homeData} />
+            {/* <HomeGlobalOutletsView theme={theme} homeData={homeData} /> */}
         </View>
     );
 };
