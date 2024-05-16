0a0cb89464fc5711a94bd034ea9330871443cc56
steeven-js <jacques.steeven@gmail.com>
2024-05-15 21:32:29 -0400

    chore: Update styles.js with leftArrowIcon and rightIcon

--- src/navigators/stacks/styles.js
+++ src/navigators/stacks/styles.js
@@ -1,17 +1,21 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
+
 import {
-  FONT_SIZE_MD,
-  STANDARD_SPACING,
-  POPPINS_SEMIBOLD,
+    FONT_SIZE_MD,
+    POPPINS_SEMIBOLD,
+    STANDARD_SPACING,
 } from '../../config/Constants';
 
 // Exporting style
 export default StyleSheet.create({
-  leftArrowIcon: {
-    marginLeft: STANDARD_SPACING * 3,
-  },
-  headerTitle: {
-    fontFamily: POPPINS_SEMIBOLD,
-    fontSize: FONT_SIZE_MD,
-  },
+    leftArrowIcon: {
+        marginLeft: STANDARD_SPACING * 3,
+    },
+    rightIcon: {
+        marginRight: STANDARD_SPACING * 3,
+    },
+    headerTitle: {
+        fontFamily: POPPINS_SEMIBOLD,
+        fontSize: FONT_SIZE_MD,
+    },
 });
