fb8905a101702451c5eb505804d5ffea02f0fb14
steeven-js <jacques.steeven@gmail.com>
2024-05-31 15:48:57 -0400

    chore: Update dependencies and enable static framework for RNAdMob
    Update the dependencies in package.json to include "@react-native-admob/admob" version "^2.0.1". Also, enable the static framework for RNAdMob in the Podfile by setting "$RNAdMobAsStaticFramework" to true. This change ensures that the latest version of RNAdMob is used and improves the performance of the app.

--- App.jsx
+++ App.jsx
@@ -1,16 +1,23 @@
-import { Provider } from 'react-redux';
-import Splash from './src/screens/Splash';
 import React, { useEffect, useState } from 'react';
-import Statusbar from './src/components/others/Statusbar';
+import { Platform } from 'react-native';
+import { Provider } from 'react-redux';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { ThemeContextProvider } from './src/theming/context_providers/ThemeContextProvider';
 import store from './src/redux/store';
 import Toast from 'react-native-toast-message';
+import Splash from './src/screens/Splash';
 import Welcome from './src/screens/Welcome/Index';
 import LaunchApp from './src/screens/launchApp';
+import Statusbar from './src/components/others/Statusbar';
+import { BannerAd, BannerAdSize } from '@react-native-admob/admob';
 
 const Stack = createStackNavigator();
+
+// AdMob unit IDs
+const adUnitId = Platform.OS === 'ios'
+  ? 'ca-app-pub-2536948516491703/5832444748'
+  : 'ca-app-pub-2536948516491703/8832931036';
 
 // Functional component
 const App = () => {
@@ -38,12 +45,16 @@ const App = () => {
       <ThemeContextProvider>
         <NavigationContainer>
           <Statusbar barStyle="light-content" />
-            <Stack.Navigator initialRouteName="Welcome" screenOptions={{
-              headerShown: false,
-            }}>
-              <Stack.Screen name="Welcome" component={Welcome} />
-              <Stack.Screen name="LaunchApp" component={LaunchApp} />
-            </Stack.Navigator>
+          <Stack.Navigator initialRouteName="Welcome" screenOptions={{
+            headerShown: false,
+          }}>
+            <Stack.Screen name="Welcome" component={Welcome} />
+            <Stack.Screen name="LaunchApp" component={LaunchApp} />
+          </Stack.Navigator>
+            <BannerAd
+              unitId={adUnitId}
+              size={BannerAdSize.ADAPTIVE_BANNER}
+            />
           <Toast />
         </NavigationContainer>
       </ThemeContextProvider>
